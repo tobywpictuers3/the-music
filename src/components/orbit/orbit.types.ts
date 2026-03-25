cbe61722e4f858df15ccdca76c78f297901f1e89
tobywgibui <tobyw.gibui@gmail.com>
2026-03-25 16:28:46 +0200

    Update orbit.types.ts3

--- src/components/orbit/orbit.types.ts
+++ src/components/orbit/orbit.types.ts
@@ -1,11 +1,21 @@
-export type AvatarKey =
-  | "front"
+export type LegacyAvatarKey =
   | "leftMid"
   | "leftSide"
   | "rightMid"
   | "rightSide"
   | "up"
   | "down";
+
+export type PresenterPoseKey =
+  | "front"
+  | "upRight"
+  | "right"
+  | "downRight"
+  | "downLeft"
+  | "left"
+  | "upLeft";
+
+export type AvatarKey = PresenterPoseKey | LegacyAvatarKey;
 
 export type OrbitItem = {
   id: string;
@@ -14,6 +24,18 @@ export type OrbitItem = {
   orbitTitle: string;
   orbitNote: string;
   sectionId?: string;
+  href?: string;
 };
 
-export type PresenterAssets = Record<AvatarKey, string>;
+export type PresenterAssets = Partial<Record<AvatarKey, string>> & {
+  front: string;
+};
+
+export type ResolvedPresenterAssets = Record<PresenterPoseKey, string>;
+
+export type OrbitVariant = "inner" | "home";
+
+export type FloatingBubbleMessage = {
+  id: string;
+  text: string;
+};
